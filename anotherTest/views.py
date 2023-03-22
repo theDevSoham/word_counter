14648bf84c44a2819c10672372b0e2466061b7a4
Soham Das <publicsoham.24@gmail.com>
2023-03-22 12:49:48 +0530

    get changed to post

--- anotherTest/views.py
+++ anotherTest/views.py
@@ -7,6 +7,6 @@ def index(request):
 	return render(request, 'index.html')
 
 def counter(request):
-	text = request.GET['text']
+	text = request.POST['text']
 	no_of_words = len(text.split())
 	return render(request, 'counter.html', {'counter': no_of_words})
